42a8b76dc54b855de2bf7f6e2a1573fe999b18ee
madokainagaki <jo6gencho9@gmail.com>
2022-05-27 23:48:13 +0900

    横スライドするタブのレイアウトまで

---
+++ app/src/main/java/jp/techacademy/madoka/inagaki/apiapp/ApiFragment.kt
@@ -0,0 +1,6 @@
+package jp.techacademy.madoka.inagaki.apiapp
+
+import androidx.fragment.app.Fragment
+
+class ApiFragment: Fragment() {
+}
---
+++ app/src/main/java/jp/techacademy/madoka/inagaki/apiapp/FragmentStateAdapter.kt
@@ -0,0 +1,8 @@
+package jp.techacademy.madoka.inagaki.apiapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+class FragmentStateAdapter{
+
+}
